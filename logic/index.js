059c71f136294e5ec7a21c69bdd582d4bfe50af0
Kyle Foster <40616718+Foscat@users.noreply.github.com>
2018-11-05 20:07:13 -0600

    in working order

--- logic/index.js
+++ logic/index.js
@@ -1,22 +1,15 @@
-//Currently does not work it does not ask the questions or accept inputs..
-//It currently displays guesses and underscores in a infinite loop.
-// im still debugging things and hope to have it working soon. Time got away from me so i did not have the time to focus and grind
-// in this one like i wanted to. The tsets for the required other filees work fine but it was the bringing it all together is difficult
-
-
-
-var Word = require("./Word");
-var inquirer = require("inquirer");
-//var prompt = require("prompt")
+var Word = require('./Word.js');
+var inquirer = require('inquirer');
 
 
 // global empty placeholder objects
 var current;
 var currentWord;
-guesses = [];
-guessesLeft = 9;
+var guesses;
+var guessesLeft;
 
-// array of unique names from differant movies
+
+    // array of unique names from differant movies
 var movieNames = [
     "Breathless Mahoney", "Steve Stifler", "Dirk Diggler",
     "Honey Rider", "Gaylord Focker", "Flipper Purify",
@@ -29,147 +22,131 @@ var movieNames = [
     "Norville Roberts", "Taco Macarthur", "Steve and Doug Butabi"
 ];
 
-// random math to provide varity in the game
-randomWord = function(movieNames){
-    var index = Math.floor(Math.random() * movieNames.length);
-    return movieNames[index];
+    // random math that chooses a rondom word each time to provide varity in the game
+    function randomWord(movieNames) {
+        var index = Math.floor(Math.random() * movieNames.length);
+        return movieNames[index];
+    }
 
-};
-
-// randomWord function testing and setting of global currentWord object
-currentWord = randomWord(movieNames);
-    console.log("Current Word: " + currentWord);
-    // current is the letter array for the currentword
-    current = new Word(currentWord);
-    console.log("current")
-    console.log(current)
-
-
-//The questions are objects that contain functions in an array
+    //The questions are objects that contain functions in an array
 var gameQuestions = [
-    //first question aka start
-    {
-        name: "GuessLetter",
-        message: "Guess a letter",
-        check: function(value) {
 
-            // this var allows only letters to be accepted inputs and converst all inputs to lowercase
+    {//first question aka gameplay
+
+        name: 'GuessLetter',
+        message: 'Guess a letter',
+        validate: function (value) {
+
             // The indexOf() method returns the index within the calling String object of the first occurrence
             // of the specified value, starting the search at fromIndex. Returns -1 if the value is not found.
-            var acceptedInputs = ('abcdefghijklmnopqrstuvwxyz'.indexOf(value.charAt(0).toLowerCase()) !== -1)
+            // This var does the comparison of user input to the accepted inputs index and allowing only one input at a time
+            var valid = (value.length === 1) && ('abcdefghijklmnopqrstuvwxyz'.indexOf(value.charAt(0).toLowerCase()) !== -1);
+            return valid || 'Please enter only a single letter at a time.';
 
-            // does the comparison of user input to the accepted inputs index
-            var checked = (value.length === 1) && acceptedInputs;
-            return checked || "Type a letter"
-            },
-
-            // when it it checked it returns the whole thing if you still have guesses left
-            when: function () {
-                return (current.allCorrect() && guessesLeft > 0);
-            }
-
-    },
-    //second question aka restart
-    {
-        type: "confirm",
-        name: "newGame",
-        message: "Play Again?",
-
-        when: function() {   
-            // 
-            return(current.allCorrect() || guessesLeft <= 0); 
+        },// when it is checked it returns the whole thing if you still have guesses left
+        when: function () {
+            return (!current.allCorrect() && guessesLeft > 0);
         }
 
+    },
+
+    {//second question aka restart
+        type: 'confirm',
+        name: 'newGame',
+        message: 'Play again?',
+        // default: true,
+        when: function () {
+            return (current.allCorrect() || guessesLeft <= 0);
+        }
     }
-];// end of game question array
+];
 
 ///////////////////////////////////////////////
 
 // resets the game to defaults
-var restartGame = function(){
+var resetGame = function() {
     currentWord = randomWord(movieNames);
+    // console.log(currentWord); //for cheating and debugging
     current = new Word(currentWord);
-    current.inputGuess(" ");
+    current.inputGuess(' ');
     guesses = [];
-    guessesLeft = 9;
-    askQuestion();
-    // check that a new word has gone through
-    console.log(currentWord);
-};
+    guessesLeft = 7;
+}
 ///////////////////////////////////////////////
 
 // start of total game flow function
-var askQuestion = function(){
-    
-    // if the new Word  is not all correct display underscores for unsolved letters
-    if(!current.allCorrect() && guessesLeft > 0) {
-        console.log(current + "");
-         
+var askQuestion = function() {
+
+    // underscores in place of unguessed letters are dispalyed if the users still has guesses left
+    if (!current.allCorrect() && guessesLeft > 0) {
+        console.log(current + '');
     }
     
-    // ask the user the questions using the inquierer npm to start or end a game session
+    // ask the user the questions using the inquierer npm to start a new game or end a game session
     inquirer.prompt(gameQuestions).then(answers => {
-        console.log("answers");
-        console.log(answers);
 
         // if a user chooses to not play a new game give response and end the game
         // The in operator returns true if the specified property is in the specified object or its prototype chain.
-        if("newGame" in answers && !answers.newGame) {
+        if ('newGame' in answers && !answers.newGame) {
             console.log("Smell ya later!");
             process.exit();
         }
-
-        // if user chooses to play a new game 
-        if(answers.newGame) {
-            restartGame();
+        // if user chooses to play a new game
+        if (answers.newGame) {
+            resetGame();
         }
 
         //The hasOwnProperty() method returns a boolean indicating whether the object has 
         //the specified property as its own property (as opposed to inheriting it).
-        if (answers.hasOwnProperty("GuessLetter")) {
-
+        if (answers.hasOwnProperty('GuessLetter')) {
             var input = answers.GuessLetter.toLowerCase();
-
-            if(guesses.indexOf(input) === -1) {
-                // for correct inputs
+            
+            // for correct inputs
+            if (guesses.indexOf(input) === -1) {
+                // push input into guessed letters array
                 guesses.push(input);
                 current.inputGuess(input);
+
                 // for wrong inputs
                 if (currentWord.toLowerCase().indexOf(input.toLowerCase()) === -1) {
                     guessesLeft--;
                 }
+
                 // for repeat inputs
-            }else{
+            } else {
                 console.log("You already guessed that one!", input);
+                
             }
         }
 
         // if allCorrect() is false
-        if(!current.allCorrect()) {
-            // and if your total guesses is less than 1 you get this response
-            if(guessesLeft < 1) {
-                console.log("You are outta guesses Buccko!!!");
-                console.log(currentWord, " was the word you needed to spell.")
+        if (!current.allCorrect()) {
 
-             // otherwise update guesses clientside of what happened in input function above
-            }else {
-                console.log("Total guesses: ", guesses);
-                console.log("Guesses remaining: ", guessesLeft);
+            // and if your total guesses is less than 1 you get this response
+            if (guessesLeft < 1) {
+                console.log("You are outta guesses Buccko!!!");
+                console.log(currentWord, "was the word you needed to spell.");
+
+            // otherwise update guesses clientside of what happened in input function above
+            } else {
+                console.log("Total guessed letters: ", guesses.join(' '));
+                console.log('Guesses remaining:', guessesLeft);
             }
 
-         // if allCorrect() is true before you run out of guesses    
-        }else {
+        // if allCorrect() is true before you run out of guesses
+        } else {
             console.log("Congratulations!!!");
             console.log(currentWord + " was the correct answer!");
+            
         }
 
-        // call to ask a new random word to spell
+        // call to ask you to input another letter
         askQuestion();
 
     });// end of inquirer prompt function
 
 }// end of general game flow function
- 
-  askQuestion();
-  
 
+resetGame();
+askQuestion();
+// console.log(currentWord); //for cheating and debugging
